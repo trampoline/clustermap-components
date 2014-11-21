d58b9d5f85833b3ea7cd13e25abebc225b78de0a
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2014-11-21 17:55:12 +0000

    update deps

--- project.clj
+++ project.clj
@@ -1,16 +1,18 @@
 (defproject clustermap-components "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
+  :description "om components"
+  :url "https://github.com/trampoline/clustermap-components"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2356" :scope "provided"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha" :scope "provided"]
-                 [domina "1.0.2"]
-                 [om "0.8.0-alpha1"]
+                 [om "0.8.0-alpha2"]
                  [prismatic/om-tools "0.3.6" :exclusions [org.clojure/clojure]]
+                 [domina "1.0.3"]
                  [sablono "0.2.22"]
                  [hiccups "0.3.0"]
                  [secretary "1.2.0"]
-                 [com.andrewmcveigh/cljs-time "0.2.2"]]
+                 [com.andrewmcveigh/cljs-time "0.2.4"]
+                 [jayq "2.5.2"] ;; let's get rid of this soon
+                 ]
 
   :source-paths ["src"])
