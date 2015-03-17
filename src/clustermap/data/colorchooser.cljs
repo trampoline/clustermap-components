f4a27b35332d354a2da96b16e1adb67fe6310d34
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-03-17 17:23:31 +0000

    a color scale which ignores outliers

--- src/clustermap/data/colorchooser.cljs
+++ src/clustermap/data/colorchooser.cljs
@@ -7,8 +7,27 @@
   [steps data]
   (let [sorted-data (->> data (filter identity) sort vec)
         point-count (count sorted-data)]
+
     (->> (range 1 steps)
          (map (fn [i] (get sorted-data (int (/ (* point-count i) steps))))))))
+
+(defn auto-no-outliers-scale
+  "chooses scale points based on the data with outliers filtered by interquartile range
+   k - only use points in range [Q1 - k*(Q3-Q1) , Q3 + k*(Q3-Q1)]"
+  [steps k data]
+  (let [sorted-data (->> data (filter identity) sort vec)
+        point-count (count sorted-data)
+        q1 (get sorted-data (int (/ (* point-count 1) 4)))
+        q3 (get sorted-data (int (/ (* point-count 3) 4)))
+        iq (- q3 q1)
+        filtered-data (->> sorted-data
+                           (filter (fn [d] (and (>= d (- q1 (* k iq)))
+                                                (<= d (+ q3 (* k iq))))))
+                           vec)
+        filtered-point-count (count filtered-data)]
+
+    (->> (range 1 steps)
+         (map (fn [i] (get filtered-data (int (/ (* filtered-point-count i) steps))))))))
 
 (defn linear-scale
   "returns a list of linear-scale thresholds"
@@ -51,7 +70,8 @@
         thresholds (condp = scale
                      :log (log-scale min-value max-value col-count)
                      :linear (linear-scale min-value max-value col-count)
-                     :auto (auto-scale col-count values))
+                     :auto (auto-scale col-count values)
+                     :auto-no-outliers (auto-no-outliers-scale col-count 5 values))
         chooser (partial choose-from-scheme color-scheme thresholds)
         value-colors (->> data
                           (map (fn [r]
