7a38a193d0b5971a81f37798b68d0d318e827760
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-03-17 16:27:54 +0000

    an n-tile color scale chooser
    and various fixes for log-sccale colors and values in the last cell

--- src/clustermap/data/colorchooser.cljs
+++ src/clustermap/data/colorchooser.cljs
@@ -2,20 +2,31 @@
   (:require [clustermap.data.picker :as picker]
             [clustermap.data.colorbrewer :as colorbrewer]))
 
+(defn auto-scale
+  "chooses scale points based on the data itself... i.e. n-tile points"
+  [steps data]
+  (let [sorted-data (->> data (filter identity) sort vec)
+        point-count (count sorted-data)]
+    (->> (range 1 steps)
+         (map (fn [i] (get sorted-data (int (/ (* point-count i) steps))))))))
+
 (defn linear-scale
+  "returns a list of linear-scale thresholds"
   [min max steps]
   (let [step (/ (- max min) steps)]
     (->> (range 1 steps)
          (map (fn [i] (+ min (* i step)))))))
 
 (defn log-scale
+  "returns a list of log-scale thresholds"
   [min max steps]
   (let [translation (- 1 min) ;; translate to avoid NaNs
 
-        log-min (Math/log min)
+        log-min 0 ;; (Math/log 1)
         log-max (Math/log (+ max translation))
 
-        log-linear-scale (linear-scale 1 log-max steps)]
+        log-linear-scale (linear-scale 0 log-max steps)]
+
     (->> log-linear-scale
          (map (fn [n] (- (Math/pow Math/E n) translation))))))
 
@@ -23,10 +34,11 @@
   [scheme thresholds value]
   (let [scheme-thresholds (map vector thresholds scheme)
         chosen (->> scheme-thresholds
-                    (filter (fn [[t i]] (<= value t)))
+                    (filter (fn [[t i]] (< value t)))
                     first
                     last)]
-    (or chosen (last scheme))))
+    (or chosen
+        (last scheme))))
 
 (defn choose
   "return a map of {key-value => colours}"
@@ -36,13 +48,14 @@
         values (picker/pick-variable variable data)
         min-value (apply min values)
         max-value (apply max values)
-        thresholds (if (= scale :log)
-                     (log-scale min-value max-value col-count)
-                     (linear-scale min-value max-value col-count))
+        thresholds (condp = scale
+                     :log (log-scale min-value max-value col-count)
+                     :linear (linear-scale min-value max-value col-count)
+                     :auto (auto-scale col-count values))
         chooser (partial choose-from-scheme color-scheme thresholds)
         value-colors (->> data
                           (map (fn [r]
                                  [(get r key) (chooser (get r variable))]))
                           (into {}))
-        threshold-colors (map vector (concat thresholds [max-value]) color-scheme)]
+        threshold-colors (map vector (concat [min-value] thresholds) color-scheme)]
     [threshold-colors value-colors]))
