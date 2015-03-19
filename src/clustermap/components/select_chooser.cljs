defc630550445c742739d0aebf8cd22e87724a5c
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-03-19 15:28:04 +0000

    set option value from cursor

--- src/clustermap/components/select_chooser.cljs
+++ src/clustermap/components/select_chooser.cljs
@@ -5,15 +5,18 @@
 (defn select-chooser-component
   [title key value-descriptions {:as cursor} owner]
   (om/component
-   (html
-    [:div.select-chooser
-     [:div.tbl
-      [:div.tbl-row
-       [:div.tbl-cell title]
-       [:div.tbl-cell
-        [:select {:onChange (fn [e]
-                              (let [val (-> e .-target .-value not-empty)]
-                                (.log js/console val)
-                                (om/update! cursor key val)))}
-         (for [[value description] value-descriptions]
-           [:option {:value value} description])]]]]])))
+
+   (let [current-val (some-> cursor (get-in [key]) name)]
+     (html
+      [:div.select-chooser
+       [:div.tbl
+        [:div.tbl-row
+         [:div.tbl-cell title]
+         [:div.tbl-cell
+          [:select {:onChange (fn [e]
+                                (let [val (-> e .-target .-value not-empty)]
+                                  (.log js/console val)
+                                  (om/update! cursor key val)))
+                    :value current-val}
+           (for [[value description] value-descriptions]
+             [:option {:value (name value)} description])]]]]]))))
