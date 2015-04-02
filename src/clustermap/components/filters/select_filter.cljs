b277bcfc0472ea60d0acde4e8352de1883f20834
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-04-02 15:00:10 +0100

    show correct value on init

--- src/clustermap/components/filters/select_filter.cljs
+++ src/clustermap/components/filters/select_filter.cljs
@@ -8,23 +8,27 @@
 (defnk render*
   [[:component-spec id label options] components]
 
-  (let [options-by-id (->> options (map (fn [o] [(:id o) o])) (into {}))]
+  (let [current-filter (get-in components [id])
+        current-option-id (->> options (some (fn [o] (when (= (:filter o) current-filter) o))) :value)
+        options-by-value (->> options (map (fn [o] [(:value o) o])) (into {}))]
+    (.log js/console (clj->js ["SELECT-OPTION" id current-option-id current-filter]))
     (html
-     [:select {:onChange (fn [e]
+     [:select {:value (if current-option-id current-option-id "")
+               :onChange (fn [e]
                            (let [val (-> e .-target .-value)]
                              (.log js/console (clj->js ["SELECT-FILTER" label id val]))
                              (om/update! components [id]
-                                         (->> val (get options-by-id) :filter))))}
-      (for [{:keys [id label] :as option} options]
-        [:option {:value id} label])])))
+                                         (->> val (get options-by-value) :filter))))}
+      (for [{:keys [value label] :as option} options]
+        [:option {:value value} label])])))
 
 (def SelectFilterComponentSchema
   {:component-spec {:id s/Keyword
                     :type (s/eq :select)
                     :label s/Str
-                    :options [{:id (s/either s/Keyword s/Str)
+                    :options [{:value (s/either s/Keyword s/Str)
                                :label s/Str
-                               :filter (s/either (s/eq nil) {s/Keyword s/Any})}]}
+                               :filter (s/maybe {s/Keyword s/Any})}]}
    :components {s/Keyword s/Any}})
 
 ;; a <select> filter
@@ -32,7 +36,4 @@
   [data :- SelectFilterComponentSchema
    owner]
 
-  (render [_] (render* data))
-
-  (will-update [_ {:as next-props} {:as next-state}])
-  )
+  (render [_] (render* data)))
