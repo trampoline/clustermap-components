73b101ff07ea626d51d61721049f20ef56bf24bf
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-05-13 10:40:23 +0100

    new HTML for select

--- src/clustermap/components/select_chooser.cljs
+++ src/clustermap/components/select_chooser.cljs
@@ -8,15 +8,10 @@
 
    (let [current-val (some-> cursor (get-in [key]) name)]
      (html
-      [:div.select-chooser
-       [:div.tbl
-        [:div.tbl-row
-         [:div.tbl-cell title]
-         [:div.tbl-cell
-          [:select {:onChange (fn [e]
-                                (let [val (-> e .-target .-value not-empty)]
-                                  (.log js/console val)
-                                  (om/update! cursor key val)))
-                    :value current-val}
-           (for [[value description] value-descriptions]
-             [:option {:value (name value)} description])]]]]]))))
+      [:select {:onChange (fn [e]
+                            (let [val (-> e .-target .-value not-empty)]
+                              (.log js/console val)
+                              (om/update! cursor key val)))
+                :value current-val}
+       (for [[value description] value-descriptions]
+         [:option {:value (name value)} description])]))))
