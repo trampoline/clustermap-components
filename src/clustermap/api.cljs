6860956e7f97ae17b67e59266dde6a0cc3c7874d
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-06-05 22:18:17 +0100

    junk dumb ordered-resource concept

--- src/clustermap/api.cljs
+++ src/clustermap/api.cljs
@@ -36,41 +36,6 @@
 (defn PUT [url content & {:as opts}]
   (apply AJAX url (apply concat (merge opts {:method "PUT" :content content}))))
 
-(defn- ordered-api-results
-  "- ocomm : a channel containing [result-chans result-handler-args]
-             gather results from the one-or-more result-chans, and
-             pass them to the handler along with any result-handler-args
-   - handler : invoked with (apply handler result-or-results result-handler-args)"
-  [ocomm handler]
-  (go
-   (while true
-     (let [[rcomms result-handler-args] (<! ocomm)
-           result (if (sequential? rcomms)
-                    (loop [rem rcomms
-                           results []]
-                      (if (empty? rem)
-                        results
-                        (recur (rest rem) (conj results (when (first rem) (<! (first rem)))))))
-                    (when rcomms (<! rcomms)))]
-       ;; (.log js/console (clj->js result))
-       (apply handler result result-handler-args)))))
-
-(defn ordered-api
-  "order responses from an async API according to the order of requests
-   - request-handler: fn to send an async API request, returning a channel of a single result value,
-                      and optional additional args for the result-handler. return nil to
-                      abandon request
-   - result-handler: function of API result and optional additional args from request-handler result"
-  [request-handler result-handler]
-
-  (let [ocomm (chan (sliding-buffer 1))
-        _ (ordered-api-results ocomm result-handler)]
-    (fn [& req-args]
-      (if-let [r (apply request-handler req-args)]
-        (let [rseq (if (sequential? r) r [r])
-              [rcomm & result-handler-args] rseq]
-          (put! ocomm [rcomm result-handler-args]))))))
-
 (defn lastcall-method-impl
   "implements last-call-wins aync api-call semantics, discarding results from
    any earlier api calls
