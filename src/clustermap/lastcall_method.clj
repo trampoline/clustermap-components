a4c2e6594dbcd6ac47b09fe11798fd286e0f6500
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-06-12 12:34:18 +0100

    fnk versions of lastcall-method macros

--- src/clustermap/lastcall_method.clj
+++ src/clustermap/lastcall_method.clj
@@ -29,6 +29,25 @@
           (let [valch# ~@body]
             (clustermap.lastcall-method/lastcall-method-impl in-flight-atom# valch#)))))))
 
+(defn ^:private fnk-lastcall-method*
+  "like lastcall-method* but generates a hidden plumbing fnk function with the
+   params and passes all args to it.
+  there is stil a no-args version which discards pending responses"
+  [def-sym name params-body]
+  (let [[name [params & body]] (name-with-attributes name params-body)]
+
+    `(let [in-flight-atom# (atom nil)
+           f# (plumbing.core/fnk ~params
+                                (let [valch# ~@body]
+                                  (clustermap.lastcall-method/lastcall-method-impl in-flight-atom# valch#)))]
+       (~def-sym ~name
+         ([]
+          (let [emptych# (cljs.core.async/chan)]
+            (cljs.core.async/close! emptych#)
+            (clustermap.lastcall-method/lastcall-method-impl in-flight-atom# emptych#)))
+         ([& p#]
+          (apply f# p#))))))
+
 (defmacro def-lastcall-method
   "defn a lastcall method"
   [name & params-body]
@@ -36,7 +55,7 @@
 
 (defmacro def-lastcall-method-factory
   "defn a zero-args factory for a lastcall method, which returns an instance
-   of the method when called"
+  of the method when called"
   [name & params-body]
   (let [[name [params & body]] (name-with-attributes name params-body)
         method-name (gensym name)
@@ -49,3 +68,26 @@
   "return an anonymous instance of a lastcall method"
   [name & params-body]
   (lastcall-method* 'fn name params-body))
+
+;; fnk versions of all the above
+
+(defmacro defnk-lastcall-method
+  "defnk a fnk-lastcall method"
+  [name & params-body]
+  (fnk-lastcall-method* 'defn name params-body))
+
+(defmacro defnk-lastcall-method-factory
+  "defn a zero-args factory for an fnk-lastcall method, which returns an instance
+  of the method when called"
+  [name & params-body]
+  (let [[name [params & body]] (name-with-attributes name params-body)
+        method-name (gensym name)
+        method-fn (fnk-lastcall-method* 'fn method-name params-body)]
+    `(defn ~name
+       []
+       ~method-fn)))
+
+(defmacro fnk-lastcall-method
+  "return an anonymous instance of an fnk-lastcall method"
+  [name & params-body]
+  (fnk-lastcall-method* 'fn name params-body))
