2e5fdc5399d093b2a494ccbc37d11c7b85555af6
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-09-22 12:59:47 +0100

    update clojure dep

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "om components"
   :url "https://github.com/trampoline/clustermap-components"
 
-  :dependencies [[org.clojure/clojure "1.7.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.macro "0.1.5"]
                  [org.clojure/clojurescript "0.0-3196" :scope "provided"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha" :scope "provided"]
