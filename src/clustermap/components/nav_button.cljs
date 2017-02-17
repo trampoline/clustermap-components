c989b37be6a59a6b959a32a235ea4b5ab5da64c5
Christopher McDevitt <christophermcdevitt@gmail.com>
2017-02-17 13:52:10 +0000

    feat: optional on-click-action for nav-button
    Runs a function before nave after click.
    
    To enable clearing the search box on company info close.
    
    e.g.
    
    {:on-click-action
     (fn [e app owner]
       (when-let [search-chan (om/get-shared owner :search-chan)]
         (async/put! search-chan :clear)))}

--- src/clustermap/components/nav_button.cljs
+++ src/clustermap/components/nav_button.cljs
@@ -10,18 +10,22 @@
   {:nav-button {:text s/Str
                 :target-view s/Str
                 (s/optional-key :class) s/Str
-                (s/optional-key :id) s/Str}})
+                (s/optional-key :id) s/Str
+                (s/optional-key :on-click-action) (s/pred fn?)}})
 
 (defcomponentk nav-button-component
-  [[:data [:nav-button text target-view class {id nil}]] :- NavButtonSchema
+  "On click action is an optional side-effecting fn which takes 3 args
+  e, app, owner."
+  [[:data [:nav-button text target-view class {id nil} {on-click-action nil}]] :- NavButtonSchema
    [:shared app]
-    owner]
-  (render
-   [_]
-   (html
-    [:button.btn (-> {:type "button"
-                      :onClick (fn [e]
-                                 (navigate app target-view))}
-                     (assoc-when :class class)
-                     (assoc-when :id id))
-     text])))
+   owner]
+  (render [_]
+    (html
+     [:button.btn (-> {:type "button"
+                       :onClick (fn [e]
+                                  (when on-click-action
+                                    (on-click-action e app owner))
+                                  (navigate app target-view))}
+                      (assoc-when :class class)
+                      (assoc-when :id id))
+      text])))
